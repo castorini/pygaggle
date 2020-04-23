5ef22bd95136269a82873d45aeb56209efdb44f1
Ralph Tang <r33tang@uwaterloo.ca>
2020-04-23 17:00:24 -0400

    Change license to Apache

--- setup.py
+++ setup.py
@@ -32,7 +32,7 @@ setuptools.setup(
     packages=setuptools.find_packages(),
     classifiers=[
         'Programming Language :: Python :: 3',
-        'License :: OSI Approved :: MIT License',
+        'License :: OSI Approved :: Apache 2.0 License',
         'Operating System :: OS Independent',
     ],
     python_requires='>=3.7',
