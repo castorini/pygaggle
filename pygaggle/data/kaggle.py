Fix off-by-one in random MRR computation

pygaggle/data/kaggle.py
@@ -81,9 +81,9 @@ class LitReviewDataset(BaseModel):
             n = len(int_rels) - p
             N = len(int_rels)
             mean_stats['Random R@3'].append(1 - (n * (n - 1) * (n - 2)) / (N * (N - 1) * (N - 2)))
-            denom = np.array([sp.comb(N, i) for i in range(1, n + 1)])
-            numer = np.array([sp.comb(n, i) / (n - i + 1) for i in range(1, n + 1)])
-            rr = 1 / np.arange(1, n + 1)
+            numer = np.array([sp.comb(n, i) / (N - i) for i in range(0, n + 1)]) * p
+            denom = np.array([sp.comb(N, i) for i in range(0, n + 1)])
+            rr = 1 / np.arange(1, n + 2)
             rmrr = np.sum(numer * rr / denom)
             mean_stats['Random MRR'].append(rmrr)
             if not any(rels):