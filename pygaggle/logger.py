cad894425c7338ebbe03beae692ede519344cb35
ronakice <ronakice@hotmail.com>
2020-12-02 23:38:08 +0530

    clean filelock output

--- pygaggle/logger.py
+++ pygaggle/logger.py
@@ -8,3 +8,4 @@ __all__ = []
 coloredlogs.install(level='INFO',
                     fmt='%(asctime)s [%(levelname)s] %(module)s: %(message)s')
 logging.getLogger("transformers").setLevel(logging.WARNING)
+logging.getLogger("filelock").setLevel(logging.WARNING)
