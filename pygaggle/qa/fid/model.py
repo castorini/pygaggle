3cfc4625ddad16378274cfafa4d4d302156fb043
Lingwei Gu <gulw2001@163.com>
2022-02-12 11:40:47 -0500

    refactor fid reader

--- pygaggle/qa/fid/model.py
+++ pygaggle/qa/fid/model.py
@@ -1,8 +1,7 @@
 # Copyright (c) Facebook, Inc. and its affiliates.
 # All rights reserved.
 #
-# This source code is licensed under the license found in the
-# LICENSE file in the root directory of this source tree.
+# This source code is licensed under the license https://github.com/facebookresearch/FiD/blob/main/LICENSE
 
 import types
 import torch
--- pygaggle/run/evaluate_fid_ranker.py
+++ pygaggle/run/evaluate_fid_ranker.py
@@ -244,3 +244,4 @@ def main():
 
 if __name__ == '__main__':
     main()
+    
